e105c7813fd096512e77e9fa33c8bb0474b35480
xylan <xylannerd@gmail.com>
2022-07-13 03:54:53 +0530

    isAppReady - userInput - addPostScreen

--- src/navigation/navigation.tsx
+++ src/navigation/navigation.tsx
@@ -11,54 +11,55 @@ import ProfileScreen from '../screens/ProfileScreen'
 import { color } from '../styles/colors'
 import * as Font from 'expo-font'
 import * as SplashScreen from 'expo-splash-screen'
+import { getFocusedRouteNameFromRoute } from '@react-navigation/native'
 
 const Tab = createBottomTabNavigator()
 const Stack = createNativeStackNavigator()
 
-let customFonts = {
-  'Splash-Regular': require('../assets/fonts/Splash-Regular.ttf'),
-}
+// let customFonts = {
+//   'Splash-Regular': require('../assets/fonts/Splash-Regular.ttf'),
+// }
 
 const RootNavigator = () => {
   // // //
-  const [appIsReady, setAppIsReady] = useState(false)
+  // const [appIsReady, setAppIsReady] = useState(false)
 
-  useEffect(() => {
-    async function prepare() {
-      try {
-        // Keep the splash screen visible while we fetch resources
-        await SplashScreen.preventAutoHideAsync()
-        // Pre-load fonts, make any API calls you need to do here
-        await Font.loadAsync(customFonts)
+  // useEffect(() => {
+  //   async function prepare() {
+  //     try {
+  //       // Keep the splash screen visible while we fetch resources
+  //       await SplashScreen.preventAutoHideAsync()
+  //       // Pre-load fonts, make any API calls you need to do here
+  //       await Font.loadAsync(customFonts)
 
-        // // Artificially delay for two seconds to simulate a slow loading
-        // // experience. Please remove this if you copy and paste the code!
-        // await new Promise(resolve => setTimeout(resolve, 2000));
-      } catch (e) {
-        console.warn(e)
-      } finally {
-        // Tell the application to render
-        setAppIsReady(true)
-      }
-    }
+  //       // // Artificially delay for two seconds to simulate a slow loading
+  //       // // experience. Please remove this if you copy and paste the code!
+  //       // await new Promise(resolve => setTimeout(resolve, 2000));
+  //     } catch (e) {
+  //       console.warn(e)
+  //     } finally {
+  //       // Tell the application to render
+  //       setAppIsReady(true)
+  //     }
+  //   }
 
-    prepare()
-  }, [])
+  //   prepare()
+  // }, [])
 
-  const onLayoutRootView = useCallback(async () => {
-    if (appIsReady) {
-      // This tells the splash screen to hide immediately! If we call this after
-      // `setAppIsReady`, then we may see a blank screen while the app is
-      // loading its initial state and rendering its first pixels. So instead,
-      // we hide the splash screen once we know the root view has already
-      // performed layout.
-      await SplashScreen.hideAsync()
-    }
-  }, [appIsReady])
+  // const onLayoutRootView = useCallback(async () => {
+  //   if (appIsReady) {
+  //     // This tells the splash screen to hide immediately! If we call this after
+  //     // `setAppIsReady`, then we may see a blank screen while the app is
+  //     // loading its initial state and rendering its first pixels. So instead,
+  //     // we hide the splash screen once we know the root view has already
+  //     // performed layout.
+  //     await SplashScreen.hideAsync()
+  //   }
+  // }, [appIsReady])
 
-  if (!appIsReady) {
-    return null
-  }
+  // if (!appIsReady) {
+  //   return null
+  // }
 
   const commonScreenOptions = {
     headerShown: false,
@@ -89,15 +90,53 @@ const RootNavigator = () => {
     )
   }
 
+  // const MockComponent = () => (
+  //   <View style={{ flex: 1, backgroundColor: 'white' }} />
+  // )
+
+  // const getHeaderTitle = (route) => {
+  //   // If the focused route is not found, we need to assume it's the initial screen
+  //   // This can happen during if there hasn't been any navigation inside the screen
+  //   // In our case, it's "Feed" as that's the first screen inside the navigator
+  //   const routeName = getFocusedRouteNameFromRoute(route) ?? 'Home'
+
+  //   switch (routeName) {
+  //     case 'Home':
+  //       return 'Home'
+  //     case 'Profile':
+  //       return 'My profile'
+  //     case 'Settings':
+  //       return 'Settings'
+  //   }
+  // }
+
+  // const getTabBarVisibility = (route) => {
+  //   // If the focused route is not found, we need to assume it's the initial screen
+  //   // This can happen during if there hasn't been any navigation inside the screen
+  //   // In our case, it's "Feed" as that's the first screen inside the navigator
+
+  //   console.log(route)
+  //   const routeName = getFocusedRouteNameFromRoute(route) ?? 'Home'
+  //   console.log(routeName)
+
+  //   if (routeName == 'AddPost') {
+  //     return 'none'
+  //   }
+  //   return 'flex'
+  // }
+
+  /* if userTokenAvailable -> move to appStack
+  else authStack */
+
   return (
     <Tab.Navigator
-      initialRouteName="HomeTab"
+      initialRouteName="AddPostTab"
       screenOptions={{
         tabBarActiveTintColor: 'plum',
         tabBarInactiveTintColor: 'lightgray',
         tabBarShowLabel: false,
         headerTitle: () => (
-          <View onLayout={onLayoutRootView}>
+          <View>
             <Text
               style={{
                 fontFamily: 'Splash-Regular',
@@ -125,13 +164,17 @@ const RootNavigator = () => {
       <Tab.Screen
         name="AddPostTab"
         component={AddPostStack}
-        options={{
+        options={({ route }) => ({
+          headerShown: false,
           tabBarLabel: 'Add Post',
           tabBarIcon: ({ focused, color, size }) => (
             <Feather name="plus-square" size={size} color={color} />
             // <AntDesign name="plussquareo" size={size} color={color}/>
           ),
-        }}
+          tabBarStyle: {
+            display: 'none',
+          },
+        })}
       />
       <Tab.Screen
         name="ProfileTab"
--- src/screens/AddPostScreen.tsx
+++ src/screens/AddPostScreen.tsx
@@ -1,12 +1,134 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  Image,
+  TextInput,
+} from 'react-native'
+import React, { useEffect, useLayoutEffect } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { commonStyles } from '../styles/commonStyles'
+import { AntDesign } from '@expo/vector-icons'
 
-const AddPostScreen = () => {
+const AddPostScreen = ({ navigation }) => {
   return (
-    <SafeAreaView style={commonStyles.defaultContainer}>
-      <Text>AddPostScreen</Text>
+    <SafeAreaView style={{flex: 1, backgroundColor: 'white'}}>
+      {/* Header -> close-button and Post Button*/}
+      <View
+        style={{
+          flexDirection: 'row',
+          width: '100%',
+          height: 64,
+          // backgroundColor: 'yellow',
+        }}
+      >
+        {/* close button -> redirects to home */}
+        <TouchableOpacity
+          style={{
+            height: '100%',
+            width: 64,
+            alignItems: 'center',
+            justifyContent: 'center',
+            // backgroundColor: 'olive',
+          }}
+          onPress={() => navigation.goBack()}
+        >
+          <AntDesign name="close" size={24} color="black" />
+        </TouchableOpacity>
+
+        {/* Post Button */}
+        <View
+          style={{
+            height: '100%',
+            width: '100%',
+            flexShrink: 1,
+            paddingRight: 16,
+            // backgroundColor: 'lightblue',
+            alignItems: 'flex-end',
+            justifyContent: 'center',
+          }}
+        >
+          <TouchableOpacity
+            style={{
+              borderRadius: 32,
+              backgroundColor: 'plum',
+              paddingVertical: 8,
+              paddingHorizontal: 16,
+            }}
+          >
+            <Text style={{ fontWeight: 'bold' }}>Post</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+
+      {/* body */}
+      <View style={{ width: '100%' }}>
+        {/* This is flexRow which will contain pfp and textInput */}
+        <View
+          style={{
+            flexDirection: 'row',
+            // backgroundColor: 'coral',
+          }}
+        >
+          {/* left-side => pfp */}
+          {/* user-avatar view */}
+          <View
+            style={{
+              // width: 64,
+              // backgroundColor: 'mediumpurple',
+              // height: 360,
+              // backgroundColor: 'white',
+              alignItems: 'center',
+              marginLeft: 8,
+              paddingTop: 16,
+            }}
+          >
+            {/* avatar-circle-pfp */}
+            <View
+              style={{
+                height: 42,
+                width: 42,
+                borderRadius: 100,
+                //   backgroundColor: 'lightgreen',
+                // backgroundColor: 'white',
+
+                alignItems: 'center',
+                justifyContent: 'center',
+                overflow: 'hidden',
+              }}
+            >
+              <Image
+                source={require('../../src/assets/kristen.jpg')}
+                style={{ height: 42, width: 42, resizeMode: 'cover' }}
+              />
+            </View>
+          </View>
+
+          {/* right-flex -> textInput */}
+          <View
+            style={{
+              width: '100%',
+              // backgroundColor: 'pink',
+              paddingLeft: 8,
+              paddingRight: 16,
+              paddingBottom: 8,
+              paddingTop: 5,
+              // height: 360,
+              flexShrink: 1,
+              // flexWrap: 'wrap',
+            }}
+          >
+            <TextInput
+              placeholder="What's happening?"
+              placeholderTextColor='gray'
+              maxLength={500}
+              multiline={true}
+              style={{ width: '100%', paddingBottom: 64, paddingTop: 16, fontSize: 18 }}
+            />
+          </View>
+        </View>
+      </View>
     </SafeAreaView>
   )
 }
